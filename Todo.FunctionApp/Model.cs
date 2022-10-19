4e5cc31289941fe258eb77ec004273a0480ddf49
Daniel Okello <43291086+okellodaniel@users.noreply.github.com>
2022-10-19 09:06:10 +0300

    Created CRUD methods on the Azure Function

--- Todo.FunctionApp/Model.cs
+++ Todo.FunctionApp/Model.cs
@@ -1,6 +1,23 @@
-﻿namespace Todo.FunctionApp;
+﻿using System;
 
-public class Model
+namespace Todo.FunctionApp;
+
+public class ToDo
 {
-    
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public DateTime CreatedTime { get; set; } = DateTime.Now;
+    public string TaskDescription { get; set; }
+    public bool IsCompleted { get; set; }
+
+}
+
+public class ToDoCreateModel
+{
+    public string TaskDescription { get; set; }
+}
+
+public class TodoUpdateModel
+{
+    public string TaskDescription { get; set; }
+    public bool IsCompleted { get; set; }
 }
--- Todo.FunctionApp/ToDo.cs
+++ Todo.FunctionApp/ToDo.cs
@@ -6,28 +6,89 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Todo.FunctionApp;
 
-public static class ToDo
+public static class ToDoApi
 {
-    [FunctionName("ToDo")]
-    public static async Task<IActionResult> RunAsync(
-        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
-    {
-        log.LogInformation("C# HTTP trigger function processed a request.");
+    private static List<ToDo>items = new List<ToDo>();
 
-        string name = req.Query["name"];
+    [FunctionName("CreateTodo")]
+    public static async Task<IActionResult> CreateTodo(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "todo")] HttpRequest req, TraceWriter log)
+    {
+        log.Info("C# a new To do item");
 
         string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-        dynamic data = JsonConvert.DeserializeObject(requestBody);
-        name = name ?? data?.name;
 
-        return name != null
-            ? (ActionResult)new OkObjectResult($"Hello, {name}")
-            : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-        
+        var input = JsonConvert.DeserializeObject<ToDoCreateModel>(requestBody);
+
+        var todo = new ToDo { TaskDescription = input.TaskDescription };
+
+        items.Add(todo);
+
+        return new OkObjectResult(todo);
+ 
+    }
+
+    [FunctionName("GetTodos")]
+    public static IActionResult GetTodos(
+        [HttpTrigger(AuthorizationLevel.Anonymous,"get",Route = "todo")]HttpRequest req, TraceWriter log)
+    {
+        log.Info("Getting todo list items");
+
+        return new OkObjectResult(items);
+    }
+
+    [FunctionName("GetTodoById")]
+    public static IActionResult GetTodoById(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo/{id}")] HttpRequest req, TraceWriter log, string id)
+    {
+        log.Info("Getting todo item");
+
+        var item = items.FirstOrDefault(t => t.Id == id);
+
+        if (item == null) throw new Exception($"Item with id: {id} not found");
+
+        return new OkObjectResult(item);
+    }
+
+    [FunctionName("UpdateTodo")]
+    public static async Task<IActionResult> UpdateTodo([HttpTrigger(AuthorizationLevel.Anonymous,"put",Route="todo/{id}")]HttpRequest req,string id,TraceWriter log)
+    {
+        log.Info("Updating todo item");
+        var todo = items.FirstOrDefault(t => t.Id == id);
+
+        if (todo == null) return new NotFoundResult();
+
+        string reqBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+        var updated = JsonConvert.DeserializeObject<TodoUpdateModel>(reqBody);
+
+        if (!string.IsNullOrEmpty(updated.TaskDescription))
+        {
+            todo.TaskDescription = updated.TaskDescription;
+        }
+
+        return new OkObjectResult(todo);
+    }
+
+    [FunctionName("DeleteTodo")]
+    public static IActionResult DeleteTodo(
+      [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "todo/{id}")] HttpRequest req, TraceWriter log, string id)
+    {
+        log.Info("Deleting todo item");
+
+        var item = items.FirstOrDefault(t => t.Id == id);
+
+        if (item == null) throw new Exception($"Item with id: {id} not found");
+
+        items.Remove(item);
+
+        return new OkResult();
     }
 }
+
